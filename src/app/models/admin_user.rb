2cda02c1c9b22c16fac3d178104018ddb0ed652d
owner <owner@localhost>
2025-11-17 14:29:28 +0900

    activeadmin動作確認

--- src/app/models/admin_user.rb
+++ src/app/models/admin_user.rb
@@ -3,4 +3,8 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email", "id", "updated_at"]
+  end
 end
