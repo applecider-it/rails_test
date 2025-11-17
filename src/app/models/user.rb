255ed42fa290e071916bbedcab07a65c21bea387
owner <owner@localhost>
2025-11-17 16:25:49 +0900

    activeadminにユーザー管理追加

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # ActiveAdmin / Ransack 用に属性を許可
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email", "id", "updated_at", "reset_password_token"]
+  end
+
+  # ActiveAdmin / Ransack 用に関連を許可
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
