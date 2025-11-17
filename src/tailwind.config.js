9645fba297403c472bb625599160c5f14c24cd74
owner <owner@localhost>
2025-11-17 11:57:23 +0900

    デザインを当てた

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./app/views/**/*.erb",          // Rails のビュー
+    "./app/helpers/**/*.rb",         // ヘルパー内の class も監視
+    "./app/javascript/**/*.{js,ts,jsx,tsx}", // Vite の JS / TS / React ファイル
+  ],
   theme: {
     extend: {},
   },
