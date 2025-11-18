580b9ce8785fa250627878f758f4062c5e714f12
owner <owner@localhost>
2025-11-18 10:23:34 +0900

    メニュー修正

--- src/app/javascript/services/application/setup-application.ts
+++ src/app/javascript/services/application/setup-application.ts
@@ -1,2 +1,7 @@
+console.log('application init');
 
-console.log('application init')
+const button = document.getElementById('app-nav-mobile-menu-button');
+const menu = document.getElementById('app-nav-mobile-menu');
+button?.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
