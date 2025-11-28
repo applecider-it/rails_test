54d7fb734cb663a3072599d1d5572368446045cf
owner <owner@localhost>
2025-11-28 22:17:23 +0900

    開発者向けページ

--- src/app/admin/development.rb
+++ src/app/admin/development.rb
@@ -5,6 +5,6 @@ ActiveAdmin.register_page "Development" do
   content title: proc { "開発者向けページ" } do
     para "ここは独自ページです。"
 
-    render partial: "admin/development/index", locals: { name: "admin react test" }
+    render partial: "admin/development/index", locals: { name: "admin test" }
   end
 end
