1522b94fda76768904d582629fdd783856b2f76a
owner <owner@localhost>
2025-12-11 09:55:47 +0900

    コンポーネントで使うcssクラス名をユニークになるように変更

--- src/app/javascript/services/ui/react/message/LoadingInline.tsx
+++ src/app/javascript/services/ui/react/message/LoadingInline.tsx
@@ -5,7 +5,7 @@ export default function LoadingInline() {
   return (
     <>
       <style>{`
-        @keyframes fadeInAccel {
+        @keyframes appUiMessageLoadingInlineFadeInAccel {
           0% { opacity: 0; transform: scale(0.5); }
           100% { opacity: 1; transform: scale(1); }
         }
@@ -15,7 +15,7 @@ export default function LoadingInline() {
         className="
           flex space-x-2 justify-center items-center my-28
           opacity-0
-          animate-[fadeInAccel_3s_cubic-bezier(0.16,1,0.3,1)_forwards]
+          animate-[appUiMessageLoadingInlineFadeInAccel_3s_cubic-bezier(0.16,1,0.3,1)_forwards]
         "
       >
         <span className="w-3 h-3 bg-gray-500 rounded-full animate-bounce"></span>
