e0db4a49d9d291fff4594df11f011238882b4728
owner <owner@localhost>
2025-12-11 09:46:36 +0900

    ローディング演出を緩やかにした

--- src/app/javascript/services/ui/react/message/LoadingInline.tsx
+++ src/app/javascript/services/ui/react/message/LoadingInline.tsx
@@ -3,10 +3,25 @@
  */
 export default function LoadingInline() {
   return (
-    <div className="flex space-x-2 justify-center items-center my-28">
-      <span className="w-2 h-2 bg-gray-500 rounded-full animate-bounce"></span>
-      <span className="w-2 h-2 bg-gray-500 rounded-full animate-bounce [animation-delay:-0.2s]"></span>
-      <span className="w-2 h-2 bg-gray-500 rounded-full animate-bounce [animation-delay:-0.4s]"></span>
-    </div>
+    <>
+      <style>{`
+        @keyframes fadeInAccel {
+          0% { opacity: 0; transform: scale(0.5); }
+          100% { opacity: 1; transform: scale(1); }
+        }
+      `}</style>
+
+      <div
+        className="
+          flex space-x-2 justify-center items-center my-28
+          opacity-0
+          animate-[fadeInAccel_3s_cubic-bezier(0.16,1,0.3,1)_forwards]
+        "
+      >
+        <span className="w-3 h-3 bg-gray-500 rounded-full animate-bounce"></span>
+        <span className="w-3 h-3 bg-gray-500 rounded-full animate-bounce [animation-delay:-0.2s]"></span>
+        <span className="w-3 h-3 bg-gray-500 rounded-full animate-bounce [animation-delay:-0.4s]"></span>
+      </div>
+    </>
   );
 }
