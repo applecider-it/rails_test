f320d69fa3c0f96db68d97172dfe706b5b4b3a0c
owner <owner@localhost>
2026-01-01 18:23:19 +0900

    cssアニメーション

--- src/app/javascript/services/ui/react/message/Loading.tsx
+++ src/app/javascript/services/ui/react/message/Loading.tsx
@@ -1,15 +1,22 @@
 /** ローディングコンポーネント */
 export default function Loading({ isLoading }: { isLoading: boolean }) {
   return (
-    <>
-      {isLoading ? (
-        <div className="fixed inset-0 bg-black bg-opacity-10 flex justify-center items-center z-50 select-none">
-          <div
-            className="w-10 h-10 border-2 border-gray-500 border-t-transparent rounded-full animate-spin shadow-md"
-            style={{ animationDuration: '0.8s' }}
-          ></div>
-        </div>
-      ) : null}
-    </>
+    <div
+      className={`
+          fixed inset-0 flex justify-center items-center z-50 select-none
+          bg-black bg-opacity-10
+          transition-opacity duration-500 ease-in-out
+          ${
+            isLoading
+              ? 'opacity-100 pointer-events-auto'
+              : 'opacity-0 pointer-events-none'
+          }
+      `}
+    >
+      <div
+        className="w-10 h-10 border-2 border-gray-500 border-t-transparent rounded-full animate-spin shadow-md"
+        style={{ animationDuration: '1.2s' }}
+      />
+    </div>
   );
 }
--- src/app/javascript/services/ui/react/popup/Modal.tsx
+++ src/app/javascript/services/ui/react/popup/Modal.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 type Props = {
   isOpen: boolean;
   onClose: () => void;
@@ -8,11 +10,22 @@ type Props = {
  * モーダルウィンドウ
  */
 export default function Modal({ isOpen, onClose, children }: Props) {
-  if (!isOpen) return null;
-
   return (
     <div
-      className="fixed inset-0 bg-black/50 flex justify-center items-center z-50"
+      className={`
+          fixed inset-0
+          bg-black/50
+          flex
+          justify-center
+          items-center
+          z-50
+          transition-opacity duration-500 ease-in-out
+          ${
+            isOpen
+              ? 'opacity-100 pointer-events-auto'
+              : 'opacity-0 pointer-events-none'
+          }
+      `}
       onClick={onClose}
     >
       <div
