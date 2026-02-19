16713f66ed0021908260ed98c17ab79803e1dd0a
owner <owner@localhost>
2026-02-19 12:58:14 +0900

    actioncable動作確認

--- src/app/channels/chat_channel.rb
+++ src/app/channels/chat_channel.rb
@@ -2,11 +2,21 @@ class ChatChannel < ApplicationCable::Channel
   def subscribed
     channel_id = "chat_channel.#{params[:room]}"
     p('subscribed channel_id', channel_id)
+    p('subscribed current_user', current_user)
+
+    @meta = {
+      channel_id: channel_id,
+      current_user_id: current_user.id,
+    }
+
+    p('subscribed @meta', @meta)
+
     stream_from channel_id
   end
 
   def unsubscribed
-    p('unsubscribed')
+    p('unsubscribed current_user', current_user)
+    p('unsubscribed @meta', @meta)
     # Any cleanup needed when channel is unsubscribed
   end
 end
