bd198dfb0d34bb4f5a98b82060f1a373202b01be
owner <owner@localhost>
2026-03-03 19:25:51 +0900

    vue除去

--- src/app/javascript/services/development/setup-javascript-test.tsx
+++ src/app/javascript/services/development/setup-javascript-test.tsx
@@ -1,10 +1,8 @@
 console.log('development page init');
 
 import { createRoot } from 'react-dom/client';
-import { createApp } from 'vue';
 
 import TestAreaReact from './react/TestAreaReact';
-import TestAreaVue from './vue/TestAreaVue.vue';
 
 let el = null;
 
@@ -18,11 +16,3 @@ if (el) {
   root.render(<TestAreaReact name={all.name} formData={all.formData} />);
 }
 
-el = document.getElementById('vue-root');
-if (el) {
-  const all: any = JSON.parse(el.dataset.all);
-
-  console.log('vue all', all);
-
-  createApp(TestAreaVue, { name: all.name }).mount(el);
-}
