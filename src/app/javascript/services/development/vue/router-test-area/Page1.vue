22d8ed8cecf8bccc2500aba7827613ec23b03c32
owner <owner@localhost>
2026-03-03 10:37:25 +0900

    js router動作確認

--- src/app/javascript/services/development/vue/router-test-area/Page1.vue
+++ src/app/javascript/services/development/vue/router-test-area/Page1.vue
@@ -7,16 +7,26 @@
     <div>
       <span>cnt: {{ cnt }}</span>
     </div>
+    <div v-if="user">
+      <span>auth user: {{ user.email }}</span>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
+import { getCurrentUser } from '@/services/application/application';
 
 const cnt = ref<number>(0);
+
+const user = ref(null);
 
 // 初期化時
 onMounted(() => {
   console.log('page1 mounted');
+
+  user.value = getCurrentUser();
+
+  console.log('user.value', user.value);
 });
 </script>
