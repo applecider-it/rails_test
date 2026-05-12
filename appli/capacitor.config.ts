5659063dc566055304cf60401872fdcb1d58328d
owner <owner@localhost>
2026-05-12 17:54:26 +0900

    android設定

--- appli/capacitor.config.ts
+++ appli/capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'myapp',
-  webDir: 'dist'
+  webDir: 'dist',
+  android: {
+    allowMixedContent: true
+  }
 };
 
 export default config;
